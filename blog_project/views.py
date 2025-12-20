b72a202598198fce5195d4896515ebb4a27455ab
Sanjay Mantati <sanjayms9510@gmail.com>
2025-12-20 10:56:27 +0530

    post by category

--- blog_project/views.py
+++ blog_project/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from blogs.models import Category , Blog
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True)
     posts = Blog.objects.filter(is_featured=False,status='published')
     context = {
-        'categories': categories,
         'featured_posts' : featured_posts,
         'posts': posts,
     }
