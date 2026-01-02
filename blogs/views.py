7eb05dc421eb1e92314222454beba42f17b88904
Sanjay Mantati <sanjayms9510@gmail.com>
2026-01-02 14:01:17 +0530

    comment feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render ,redirect ,get_object_or_404
-from .models import Blog ,Category
+from django.shortcuts import render ,redirect ,get_object_or_404, HttpResponseRedirect
+from .models import Blog ,Category,Comment
 from django.db.models import Q
 
 # Create your views here.
@@ -19,8 +19,20 @@ def posts_by_category(request,category_id):
     
 def blogs(request, slug):
     single_blog = get_object_or_404(Blog , slug=slug, status='published')
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+        
+    comments = Comment.objects.filter(blog=single_blog)
+    comment_count = comments.count()
     context = {
         'single_blog' : single_blog,
+        'comments':comments,
+        'comment_count':comment_count,
     }
     return render(request, 'blogs.html',context)
 
